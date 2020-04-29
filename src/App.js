70683ef21ebb3a61eb0ea7fb8a4d9a6c355783d5
sujilgovind <sujilgovindp@gmail.com>
2020-04-29 16:33:47 +0530

    style commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import Tree from './components/Tree';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" 
+         style={
+                {
+                  width:"40%",
+                  border: "1px solid #c7c2c2",
+                  marginTop: "20px"
+                }
+              }
+    >
       <Tree/>
     </div>
   );
